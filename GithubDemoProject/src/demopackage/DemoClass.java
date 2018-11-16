79e0dee72b6208744b55d8af2caa298af7a7d08a
Administrator <Administrator@minni-PC>
2018-11-16 17:30:55 +0530

    Added 2nd print statement

--- GithubDemoProject/src/demopackage/DemoClass.java
+++ GithubDemoProject/src/demopackage/DemoClass.java
@@ -5,5 +5,6 @@ public class DemoClass {
 	public static void main(String args[]) {
 		
 		System.out.println("I just added this line");
-	}
+		System.out.println("This is second line, added on Laxmi's laptop");	
+		}
 }
